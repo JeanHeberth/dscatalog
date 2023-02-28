e5a2a52db7b99d225dd0a1be91ca83dcf6ea0d88
Jean Heberth <jeanheberth19@gmail.com>
2023-02-28 19:39:02 -0300

    init project

--- src/main/java/com/jeanheberth/dscatalog/DscatalogApplication.java
+++ src/main/java/com/jeanheberth/dscatalog/DscatalogApplication.java
@@ -2,12 +2,21 @@ package com.jeanheberth.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DscatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
+@GetMapping("/")
+	public String  teste(){
+		return "Olá Mundão";
+	}
+
+
 
 }
